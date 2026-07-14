7aa2cb8cf7d34f5e04014073cef8a01f8ef10f2f
Suwilanji Chapewa <suwilanjichapewa@gmail.com>
2026-07-14 15:25:28 +0200

    espresso

--- Coffee machine/main.py
+++ Coffee machine/main.py
@@ -1,1 +1,22 @@
 from menu import MENU
+from menu import resources
+
+waterOg = resources["water"]
+MilkOg = resources["milk"]
+CoffeeOg = resources["coffee"]
+bank = 0
+
+decision = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+if decision == "espresso":
+    water = MENU["espresso"]["ingredients"]["water"]
+    milk = MENU["espresso"]["ingredients"]["milk"]
+    coffee = MENU["espresso"]["ingredients"]["coffe"]
+    
+    waterOg =- water
+    MilkOg =- milk
+    CoffeeOg =- coffee
+
+    print(f"current water: {waterOg}")
+    print(f"current milk: {MilkOg}")
+    print(f"current coffee: {CoffeeOg}")
