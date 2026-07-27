b484e25b6383568c68f22251b21c77ec40dc381c
Suwilanji Chapewa <suwilanjichapewa@gmail.com>
2026-07-27 22:58:02 +0200

    try

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -3,4 +3,5 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 decision = input("What would you like? (espresso/latte/cappuccino): ").lower()
-print(Menu.get_items)
+cc = Menu.get_items
+print(cc)
