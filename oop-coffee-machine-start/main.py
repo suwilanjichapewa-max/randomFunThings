1d143eb47b6d65e2d387caaf4e295590657387d4
Suwilanji Chapewa <suwilanjichapewa@gmail.com>
2026-07-29 00:02:19 +0200

    save

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -3,6 +3,3 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 decision = input("What would you like?? (espresso/latte/cappuccino): ").lower()
-cc = Menu.find_drink(decision)
-for i in cc:
-    print(i)
