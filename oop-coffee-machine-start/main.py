6d24debdd6a7a935ce2c250a58f3488dcff1733f
Suwilanji Chapewa <suwilanjichapewa@gmail.com>
2026-07-30 20:29:12 +0200

    save

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -18,3 +18,6 @@ while is_on:
         money_machine.report()
     else:
         drink = menu.find_drink(choice)
+
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+          coffee_maker.make_coffee(drink)
