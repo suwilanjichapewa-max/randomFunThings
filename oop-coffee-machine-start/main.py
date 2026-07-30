dd4c4f259e06c99ea020a7db1ebcad779925327a
Suwilanji Chapewa <suwilanjichapewa@gmail.com>
2026-07-30 20:28:09 +0200

    while

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -7,3 +7,14 @@ coffee_maker = CoffeeMaker()
 menu = Menu()
 
 is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
