28071a09fe3c34bcbe3a4151e27266ac70162144
Suwilanji Chapewa <suwilanjichapewa@gmail.com>
2026-08-04 21:11:36 +0200

    change

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -10,7 +10,7 @@ is_on = True
 
 while is_on:
     options = menu.get_items()
-    choice = input(f"What would you like? ({options}): ")
+    choice = input(f"what would you like?? ({options}): ")
     if choice == "off":
         is_on = False
     elif choice == "report":
